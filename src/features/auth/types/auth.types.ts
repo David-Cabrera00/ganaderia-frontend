290d8d07a3f3b8faef5058690347f1cee441f9e7
David-Cabrera00 <david.cabrerab@campusucc.edu.co>
2026-04-17 09:59:09 -0500

    feat: agregando types.ts

---
+++ src/features/auth/types/auth.types.ts
@@ -0,0 +1,10 @@
+PS C:\Users\davin\OneDrive\Documentos\GANADERIA_4.0\frontend> git add .
+PS C:\Users\davin\OneDrive\Documentos\GANADERIA_4.0\frontend> git commit -m "feat: add users module block 2"
+On branch feature/users-module
+Your branch is up to date with 'origin/feature/users-module'.
+
+nothing to commit, working tree clean
+PS C:\Users\davin\OneDrive\Documentos\GANADERIA_4.0\frontend> git push -u origin feature/users-module
+branch 'feature/users-module' set up to track 'origin/feature/users-module'.
+Everything up-to-date
+PS C:\Users\davin\OneDrive\Documentos\GANADERIA_4.0\frontend>
